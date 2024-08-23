b978c24d7557913455d348a05ca96f3c758a700d
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-23 16:08:21 +0100

    updated

--- src/API-ProductPreview(Test 3)/Product/ProductData.jsx
+++ src/API-ProductPreview(Test 3)/Product/ProductData.jsx
@@ -25,9 +25,7 @@ const ProductData = () => {
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-10 ">
-
       <div className="flex flex-col items-center bg-white p-4 shadow-lg rounded-lg max-w-[700px] mx-auto border-[1px] border-black">
-
         {loading ? (
           <div className="text-center">
             <p className="text-gray-500 animate-pulse">Loading...</p>
@@ -38,32 +36,40 @@ const ProductData = () => {
               {product.title}
             </h1>
 
-            <img src={product.thumbnail} alt={product.title} width={200} height={100} className="object-cover mb-4 mx-auto" />
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              width={200}
+              height={100}
+              className="object-cover mb-4 mx-auto"
+            />
 
             <p className="text-gray-600 mb-4  font-semibold text-2xl">
               Price: ${product.price}
             </p>
 
             <p className="text-gray-600 mb-4">
-              <span className="text-gray-600 font-semibold text-2xl">Description: </span>
+              <span className="text-gray-600 font-semibold text-2xl">
+                Description:{" "}
+              </span>
               {product.description}
             </p>
+
+            <div className="flex justify-between w-full max-w-sm space-x-2 mt-6">
+              <ProductButton
+                onClick={Previous}
+                buttonText={"Previous"}
+                className=" border-2 bg-gray-500 text-white font-medium py-2 px-4 rounded shadow-md hover:bg-red-300 transition duration-300"
+              />
+              <ProductButton
+                onClick={Next}
+                buttonText={"Next"}
+                className="border-2 bg-gray-500 text-white font-medium py-2 px-4 rounded shadow-md hover:bg-red-300 transition duration-300"
+              />
+            </div>
+
           </div>
         )}
-
-      </div>
-
-      <div className="flex justify-between w-full max-w-sm mt-4">
-        <ProductButton
-          onClick={Previous}
-          buttonText={"Previous"}
-          className=" border-2 bg-gray-500 text-white font-medium py-2 px-4 mr-[5rem] rounded shadow-md hover:bg-red-300 transition duration-300"
-        />
-        <ProductButton
-          onClick={Next}
-          buttonText={"Next"}
-          className="border-2 bg-gray-500 text-white font-medium py-2 px-4 rounded shadow-md hover:bg-red-300 transition duration-300"
-        />
       </div>
     </main>
   );
